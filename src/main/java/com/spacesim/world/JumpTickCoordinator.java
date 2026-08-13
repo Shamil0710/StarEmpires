9e5547c71df0392ea117186e36239b14b323ead7
Shamil0710 <69683579+Shamil0710@users.noreply.github.com>
2026-08-13 23:17:19 +0300

    chore: remove unused jump tick coordinator

--- src/main/java/com/spacesim/world/JumpTickCoordinator.java
+++
@@ -1,7 +0,0 @@
-package com.spacesim.world;
-
-/** Brings one local system simulation to an exact jump transition tick. */
-@FunctionalInterface
-interface JumpTickCoordinator {
-    void reach(StarSystemId systemId, long targetTick);
-}
