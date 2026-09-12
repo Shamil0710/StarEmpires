cb87fe08b156f7fc0a7848fe86989548a098d9eb
Shamil0710 <69683579+Shamil0710@users.noreply.github.com>
2026-09-12 14:06:22 +0300

    test: preserve deferred visual profile review boundary

--- src/test/java/com/spacesim/presentation/asset/Stage22ProductionShipVisualResolverTest.java
+++ src/test/java/com/spacesim/presentation/asset/Stage22ProductionShipVisualResolverTest.java
@@ -1,5 +1,6 @@
 package com.spacesim.presentation.asset;
 
+import com.spacesim.content.Stage22ContentGovernanceCatalog.AssetStatus;
 import com.spacesim.content.Stage22EmpirePackageCatalog;
 import com.spacesim.content.Stage22EmpirePackageLoader;
 import com.spacesim.content.Stage22IndustrialUnionPackageCatalog;
@@ -30,9 +31,11 @@ class Stage22ProductionShipVisualResolverTest {
         assertEquals("assets/ships/empire/production/freight/freight_base.png", empire.assetRef());
         assertEquals("fit.empire.freight.bulk_v1", empire.key().fitId());
         assertEquals(Stage22EmpirePackageCatalog.STABLE_FACTION_ID, empire.key().stableFactionId());
+        assertEquals(AssetStatus.CONCEPT, empire.key().shipVisualProfileStatus());
         assertEquals("assets/ships/industrial_union/production/freight/freight_base.png", union.assetRef());
         assertEquals("fit.industrial_union.freight.bulk_v1", union.key().fitId());
         assertEquals(Stage22IndustrialUnionPackageCatalog.STABLE_FACTION_ID, union.key().stableFactionId());
+        assertEquals(AssetStatus.CONCEPT, union.key().shipVisualProfileStatus());
         assertNotEquals(empire.key().shipVisualProfileId(), union.key().shipVisualProfileId());
         assertNotEquals(empire.key().packageFingerprint(), union.key().packageFingerprint());
         assertTrue(empire.worldLengthM() > 0d && empire.worldWidthM() > 0d);
