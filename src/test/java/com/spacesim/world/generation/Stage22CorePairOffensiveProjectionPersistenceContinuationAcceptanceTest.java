1b1ce987d56427b4316eee3255c3992cca482a72
Shamil0710 <69683579+Shamil0710@users.noreply.github.com>
2026-09-19 12:26:24 +0300

    Align military acceptance classification with production ship roles

--- src/test/java/com/spacesim/world/generation/Stage22CorePairOffensiveProjectionPersistenceContinuationAcceptanceTest.java
+++ src/test/java/com/spacesim/world/generation/Stage22CorePairOffensiveProjectionPersistenceContinuationAcceptanceTest.java
@@ -1,7 +1,7 @@
 package com.spacesim.world.generation;
 
 import com.badlogic.ashley.core.Entity;
-import com.spacesim.components.CombatComponent;
+import com.spacesim.components.ShipComponent;
 import com.spacesim.components.EngineeringComponent;
 import com.spacesim.components.FactionComponent;
 import com.spacesim.content.Stage22CorePairEvidenceArchive;
@@ -268,7 +268,7 @@ class Stage22CorePairOffensiveProjectionPersistenceContinuationAcceptanceTest {
             if (placement.locationKind() != FleetLocationKind.IN_SYSTEM) continue;
             Entity entity = entity(runtime, placement.id());
             FactionComponent faction = entity.getComponent(FactionComponent.class);
-            CombatComponent combat = entity.getComponent(CombatComponent.class);
+            ShipComponent ship = entity.getComponent(ShipComponent.class);
             EngineeringComponent engineering = entity.getComponent(EngineeringComponent.class);
             if (faction == null || engineering == null) continue;
             byFaction.computeIfAbsent(faction.factionId, ignored -> new ArrayList<>()).add(placement.id());
