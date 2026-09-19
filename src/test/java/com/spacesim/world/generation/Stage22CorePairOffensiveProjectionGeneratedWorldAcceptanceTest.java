c93279e527ea36162288ed93ceb690383a259dfa
Shamil0710 <69683579+Shamil0710@users.noreply.github.com>
2026-09-19 12:08:26 +0300

    Distinguish combat fleets from fitted freight in acceptance coverage

--- src/test/java/com/spacesim/world/generation/Stage22CorePairOffensiveProjectionGeneratedWorldAcceptanceTest.java
+++ src/test/java/com/spacesim/world/generation/Stage22CorePairOffensiveProjectionGeneratedWorldAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.spacesim.world.generation;
 
 import com.badlogic.ashley.core.Entity;
+import com.spacesim.components.CombatComponent;
 import com.spacesim.components.EngineeringComponent;
 import com.spacesim.components.FactionComponent;
 import com.spacesim.content.Stage22CorePairExperimentProtocol.Permutation;
@@ -263,6 +264,7 @@ class Stage22CorePairOffensiveProjectionGeneratedWorldAcceptanceTest {
             if (placement.locationKind() != FleetLocationKind.IN_SYSTEM) continue;
             Entity entity = entity(runtime, placement);
             FactionComponent faction = entity.getComponent(FactionComponent.class);
+            CombatComponent combat = entity.getComponent(CombatComponent.class);
             EngineeringComponent engineering = entity.getComponent(EngineeringComponent.class);
             if (faction == null || engineering == null) continue;
             byFaction.computeIfAbsent(faction.factionId, ignored -> new ArrayList<>()).add(placement.id());
